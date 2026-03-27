f55d6fde4ad5a700aa364d3470a1f516d39840cb
AtharvNiprul74 <atharvniprul74@gmail.com>
2026-03-27 15:04:10 +0530

    Template Engine

--- Class/Express/req-res.js
+++ Class/Express/req-res.js
@@ -2,6 +2,8 @@
 //express app handels HTTP requests and responses.
 // req = object contain information about the client request.
 // ex- URLs,Headers,Query Params,Request Body.
+// res = the response object sends data back to the client.
+// methods: 1.res.send() 2.res.json() send json. 3.res.status() check HTTP status. 4.res.sendFile()
 
 const express = require("express")
 const app = express()
@@ -9,6 +11,11 @@ const app = express()
 app.get("/",(req,res) => {
     console.log("Started ....."); 
     res.send("Welcome to request and response.")
+})
+
+app.get("/getData",(req,res) => {
+    const date = new Date()
+    res.json({mess:"Hello Welcome",Time:date})
 })
 
 //query params
