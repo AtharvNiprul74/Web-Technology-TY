c4902e068565d2a52c0c8b23a6fd27d32001f7e2
AtharvNiprul74 <atharvniprul74@gmail.com>
2026-04-13 15:28:31 +0530

    Angular binding routing Done.

--- Class/Angular/Class/First-Angular-App/src/home/home.ts
+++ Class/Angular/Class/First-Angular-App/src/home/home.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home', //name of component
-  imports: [], // for this component if any 
+  imports: [FormsModule], // for this component if any //needed to use ngModal
   templateUrl: './home.html', // html path
   styleUrl: './home.css', // css path
 })
 
 export class Home {
-  fname = "Atharv"
+  fname:String = "Atharv"
+  age:Number = 20
+  course:String = "CSE"
+
+  changeName = () => {
+    this.course = "CSE AI/ML"
+  }
 }
