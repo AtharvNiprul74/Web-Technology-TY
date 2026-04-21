39498c02bbecdd6be3e283da874b161a29355b65
AtharvNiprul74 <atharvniprul74@gmail.com>
2026-04-21 09:46:34 +0530

    Angular Routing Done.

--- Class/Angular/Class/First-Angular-App/src/app/app.ts
+++ Class/Angular/Class/First-Angular-App/src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Home } from "../home/home"
-import { AddStudent } from "../add-student/add-student"
-import { ListStudent } from '../list-student/list-student';
+import { RouterOutlet,RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, AddStudent,ListStudent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
