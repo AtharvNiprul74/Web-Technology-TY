695902e3675813f87353e32127030b51fc13d14a
AtharvNiprul74 <atharvniprul74@gmail.com>
2026-04-23 13:32:07 +0530

    React Routing Done .....

--- Class/React/StudentApp/src/App.jsx
+++ Class/React/StudentApp/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Student from './Components/Student'
+
 import './App.css'
+import { BrowserRouter, Route ,Routes } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import NavBar from './Components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <Student name="Atharv" age={20}/>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+        {/* <Student name="Atharv" age={20}/> */}
+      </BrowserRouter>
     </>
   )
 }
